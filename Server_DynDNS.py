d78ebc0a7f55e75f3c3a215ede084acf50576cc1
dvnoskov <dmytri.vik.noskov@gmail.com>
2019-12-08 01:00:01 +0200

    test update2

--- Server_DynDNS.py
+++ Server_DynDNS.py
@@ -8,9 +8,6 @@ from threading import Thread
 import concurrent.futures
 
 
-sel = selectors.DefaultSelector()
-
-
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
    # print("accepted connection from", addr)
@@ -19,38 +16,39 @@ def accept_wrapper(sock):
     sel.register(conn, selectors.EVENT_READ, data=message)
 
 
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-lsock.bind((host_DNS, port_DYN_DNS))
-lsock.listen()
-#print("listening on", (host_DNS, port_DYN_DNS))
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
-concurrent.futures.ThreadPoolExecutor(max_workers=max_pool)
-pipeline = queue.Queue(maxsize=max_queue)
+if __name__ == '__main__':
+    sel = selectors.DefaultSelector()
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    lsock.bind((host_DNS, port_DYN_DNS))
+    lsock.listen()
+    # print("listening on", (host_DNS, port_DYN_DNS))
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+    concurrent.futures.ThreadPoolExecutor(max_workers=max_pool)
+    pipeline = queue.Queue(maxsize=max_queue)
 
+    try:
+        while True:
+            loger = logging.getLogger()
+            loger.setLevel(logging.DEBUG)
+            h = logging.handlers.RotatingFileHandler("listen_dyn_dns_log.out", 300, 10)
+            loger.addHandler(h)
 
-try:
-    while True:
-        loger = logging.getLogger()
-        loger.setLevel(logging.DEBUG)
-        h = logging.handlers.RotatingFileHandler("listen_dyn_dns_log.out", 300, 10)
-        loger.addHandler(h)
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    p = Thread(target=accept_wrapper(key.fileobj), args=pipeline)
+                    p.start()
+                    #  accept_wrapper(key.fileobj)
+                else:
+                    message = key.data
+                    try:
+                        message.process_events(mask)
+                    except Exception:
+                        loger.debug("incoming message :" + str(message) + time.ctime())
+                        loger.debug(logging.exception(IndexError))
+                        message.close()
 
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                p = Thread(target=accept_wrapper(key.fileobj), args=pipeline)
-                p.start()
-                #  accept_wrapper(key.fileobj)
-            else:
-                message = key.data
-                try:
-                    message.process_events(mask)
-                except Exception:
-                    loger.debug("incoming message :" + str(message) + time.ctime())
-                    loger.debug(logging.exception(IndexError))
-                    message.close()
-
-finally:
-    sel.close()
+    finally:
+        sel.close()
